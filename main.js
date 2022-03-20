80a82df0b76ea28a38e18f07d2ca6b7fd717a30d
AaryaCoder <84276676+AaryaCoder@users.noreply.github.com>
2022-03-20 17:04:54 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ function gotResult(error, results){
     console.error(error);
   }
   else{
-    if((results[0].confidence > 0.5) && (precious_result != results[0].label)){
+    if((results[0].confidence > 0.5) && (previous_result != results[0].label)){
       console.log(results);
       previous_result= results[0].label;
       var synth = window.speechSynthesis;
